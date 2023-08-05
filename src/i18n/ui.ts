6355bb212c8ad89f85ac4547a333b4d8c3ce6012
Jakub Jirous <info@jakubjirous.cz>
2023-08-04 17:03:31 -0700

    feat(illustration): add translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -28,6 +28,16 @@ export const ui: TranslationObject = {
 
     "404.title": "Page Not Found",
 
+    "illustration.first.heading": "Passionate",
+    "illustration.first.content":
+      "Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.",
+    "illustration.second.heading": "Resourceful",
+    "illustration.second.content":
+      "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.",
+    "illustration.third.heading": "Friendly",
+    "illustration.third.content":
+      " We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.",
+
     "get-in-touch.btn.url": "contact",
     "get-in-touch.btn.aria": "Get In Touch",
     "get-in-touch.btn.title": "Get In Touch",
